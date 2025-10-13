392a50042d1458bf7d84d82342fe41f1e0e24298
thbueno <thiagobueno.s@gmail.com>
2025-10-13 12:34:20 +0700

    Add Tamagui Lucide Icons package and implement sign-in screen with navigation tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,28 +1,37 @@
-import '../tamagui-web.css'
+import "../tamagui-web.css";
 
-import { ClerkProvider } from '@clerk/clerk-expo'
-import { tokenCache } from '@clerk/clerk-expo/token-cache'
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
-import { Stack } from 'expo-router'
-import { useColorScheme } from 'react-native'
-import { TamaguiProvider } from 'tamagui'
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { Stack } from "expo-router";
+import { useColorScheme } from "react-native";
+import { TamaguiProvider } from "tamagui";
 
-import { tamaguiConfig } from '../tamagui.config'
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { tamaguiConfig } from "../tamagui.config";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme()
+  const colorScheme = useColorScheme();
 
   return (
     // add this
-    <ClerkProvider tokenCache={tokenCache}>
-    <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme!}>
-      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-        </Stack>
-      </ThemeProvider>
-    </TamaguiProvider>
-    </ClerkProvider>
-  )
+    <SafeAreaProvider>
+      <ClerkProvider tokenCache={tokenCache}>
+        <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme!}>
+          <ThemeProvider
+            value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+          >
+            <Stack>
+              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+              <Stack.Screen name="modal" options={{ presentation: "modal" }} />
+            </Stack>
+          </ThemeProvider>
+        </TamaguiProvider>
+      </ClerkProvider>
+    </SafeAreaProvider>
+  );
 }
